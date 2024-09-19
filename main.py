ffdc2a64f2612382233363a17cb23d4e023669a7
camsexsmith <155179899+camsexsmith@users.noreply.github.com>
2024-09-19 12:09:29 -0400

    more changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
-import math
 import numpy as np
 import InjectorClass as IN
+import matplotlib.pyplot as plt
 
 
 with open("input.json","r") as JSON:
@@ -12,19 +12,24 @@ InjObj = IN.Inj(inData)
 T0 = 300
 dt = 0.1
 
-A = 0.01*0.01
-V = InjObj.thick*A
-
 R = 1/InjObj.HHot + InjObj.thick/InjObj.K + 1/InjObj.HCold
 
-qpp = (InjObj.CombTemp - InjObj.FuelTemp) / R
-
-tspan = np.arange(0,15,dt)
+tspan = np.arange(0,20,dt)
 
 T = [T0]
 for n in range(np.size(tspan)):
 
-    Tnew = T[n] + qpp*A*dt/(InjObj.rho*V*InjObj.C)
+    qpp = (InjObj.CombTemp - InjObj.FuelTemp) / R
+    Tnew = T[n] + qpp*dt/(InjObj.rho*InjObj.thick*InjObj.C)
     T.append(Tnew)
+    InjObj.FuelTemp = InjObj.FuelTemp + 3
 
-print(T)
+T.pop()
+
+fig1, ax1 = plt.subplots()
+ax1.plot(tspan,T)
+ax1.set_title('Injector Temperature vs Time')
+ax1.set_ylabel('Temperature [K]')
+ax1.set_xlabel('Time [s]')
+ax1.grid(True)
+plt.show()
